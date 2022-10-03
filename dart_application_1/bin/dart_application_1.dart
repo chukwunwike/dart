260ae951ef4bbe96865af725e7e0c456ffa329ec
chukwunwike <38175567+chukwunwike@users.noreply.github.com>
2022-10-03 14:42:03 +0100

    bin/dart_application_1: create

---
+++ dart_application_1/bin/dart_application_1.dart
@@ -0,0 +1,6 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+void main(List<String> arguments) {
+
+  print('Hello world: ${dart_application_1.calculate()}!');
+}
